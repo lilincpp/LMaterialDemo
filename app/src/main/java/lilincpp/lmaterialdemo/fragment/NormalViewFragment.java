44df4af2f6a522b403f8f16b6d405c518d5d995d
lilin <lin.li@fgecctv.com>
2016-10-18 13:39:09 +0800

    增加 ProgressDrawable控件

--- app/src/main/java/lilincpp/lmaterialdemo/fragment/NormalViewFragment.java
+++ app/src/main/java/lilincpp/lmaterialdemo/fragment/NormalViewFragment.java
@@ -1,5 +1,8 @@
 package lilincpp.lmaterialdemo.fragment;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,9 +10,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import lilincpp.lmaterialdemo.R;
+import lilincpp.lmaterialdemo.view.MaterialProgressDrawable;
 
 /**
  * Created by lilin on 2016/7/21.
@@ -51,6 +57,89 @@ public class NormalViewFragment extends Fragment {
                 mLoved = !mLoved;
             }
         });
+        setupMaterialProgress(view);
 
+    }
+
+    MaterialProgressDrawable p1, p2, p3;
+
+    private void setupMaterialProgress(View view) {
+
+        final int backgroundColor = 0xFFFAFAFA;
+
+        ImageView imageView1 = (ImageView) view.findViewById(R.id.p1);
+        ImageView imageView2 = (ImageView) view.findViewById(R.id.p2);
+        ImageView imageView3 = (ImageView) view.findViewById(R.id.p3);
+
+        p1 = new MaterialProgressDrawable(getActivity(), imageView1);
+        p2 = new MaterialProgressDrawable(getActivity(), imageView2);
+        p3 = new MaterialProgressDrawable(getActivity(), imageView3);
+
+        //设置圈圈的各种大小
+        p1.setBackgroundColor(backgroundColor);
+        p2.setBackgroundColor(backgroundColor);
+        p3.setBackgroundColor(backgroundColor);
+
+        p1.setColorSchemeColors(new int[]{Color.BLUE, Color.RED, Color.GREEN, Color.YELLOW});
+        p1.updateSizes(MaterialProgressDrawable.LARGE);
+
+        p2.setColorSchemeColors(new int[]{Color.BLUE, Color.RED, Color.GREEN, Color.YELLOW});
+        p2.updateSizes(MaterialProgressDrawable.DEFAULT);
+
+        p3.setColorSchemeColors(new int[]{Color.BLUE, Color.RED, Color.GREEN, Color.YELLOW});
+        p3.updateSizes(MaterialProgressDrawable.DEFAULT);
+
+        imageView1.setImageDrawable(p1);
+        imageView2.setImageDrawable(p2);
+        imageView3.setImageDrawable(p3);
+
+//        start();
+        p1.start();
+        p2.start();
+        p3.start();
+    }
+
+    private ValueAnimator valueAnimator;
+
+    private void start() {
+        if (valueAnimator == null) {
+            valueAnimator = valueAnimator.ofFloat(0f, 1f);
+            valueAnimator.setDuration(600);
+//            valueAnimator.setRepeatMode(ValueAnimator.RESTART);
+            valueAnimator.setInterpolator(new DecelerateInterpolator());
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    float n = (float) animation.getAnimatedValue();
+                    //圈圈的旋转角度
+                    p1.setProgressRotation(n * 0.5f);
+                    p2.setProgressRotation(n * 0.5f);
+                    p3.setProgressRotation(n * 0.5f);
+                    //圈圈周长，0f-1F
+                    p1.setStartEndTrim(0f, n * 0.8f);
+                    p2.setStartEndTrim(0f, n * 0.8f);
+                    p3.setStartEndTrim(0f, n * 0.8f);
+                    //箭头大小，0f-1F
+                    p1.setArrowScale(n);
+                    //透明度，0-255
+                    p1.setAlpha((int) (255 * n));
+                    p2.setAlpha((int) (255 * n));
+                    p3.setAlpha((int) (255 * n));
+
+                }
+            });
+            valueAnimator.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                }
+            });
+        }
+
+        if (!valueAnimator.isRunning()) {
+            p1.showArrow(true);
+            valueAnimator.start();
+
+        }
     }
 }
