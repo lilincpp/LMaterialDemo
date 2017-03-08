7768cd70fc40dccfc8221836094fd74b093cf4ad
lilin <lin.li@fgecctv.com>
2017-03-08 15:25:20 +0800

    # 更改共享动画

--- app/src/main/java/lilincpp/lmaterialdemo/shareAnim/FullScreenActivity2.java
+++ app/src/main/java/lilincpp/lmaterialdemo/shareAnim/FullScreenActivity2.java
@@ -1,13 +1,10 @@
 package lilincpp.lmaterialdemo.shareAnim;
 
-import android.animation.ObjectAnimator;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.transition.Transition;
 import android.widget.ImageView;
 
 import lilincpp.lmaterialdemo.R;
@@ -27,39 +24,9 @@ public class FullScreenActivity2 extends AppCompatActivity {
 
         mPhoto = (ImageView) findViewById(R.id.photo);
 
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.photo4);
+        final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.photo4);
         mPhoto.setImageBitmap(bitmap);
 
-        getWindow().getEnterTransition().addListener(new Transition.TransitionListener() {
-            @Override
-            public void onTransitionStart(Transition transition) {
-
-            }
-
-            @Override
-            public void onTransitionEnd(Transition transition) {
-                ObjectAnimator color = ObjectAnimator.ofArgb(mPhoto.getDrawable(), "tint",
-                        ActivityCompat.getColor(FullScreenActivity2.this, R.color.tint), 0);
-                color.start();
-
-                getWindow().getEnterTransition().removeListener(this);
-            }
-
-            @Override
-            public void onTransitionCancel(Transition transition) {
-
-            }
-
-            @Override
-            public void onTransitionPause(Transition transition) {
-
-            }
-
-            @Override
-            public void onTransitionResume(Transition transition) {
-
-            }
-        });
     }
 
 //    @Override
