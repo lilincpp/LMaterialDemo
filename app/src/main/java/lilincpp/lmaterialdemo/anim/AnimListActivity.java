0b72c490b4cfb88115e6a11d10ea45298ebc9e8c
木子木木 <colin@muzimumu.local>
2017-06-22 15:24:15 +0800

    Update

---
+++ app/src/main/java/lilincpp/lmaterialdemo/anim/AnimListActivity.java
@@ -0,0 +1,20 @@
+package lilincpp.lmaterialdemo.anim;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+
+/**
+ * Created by colin on 2017/6/16.
+ */
+
+public class AnimListActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
